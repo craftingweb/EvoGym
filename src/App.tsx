2fbdd18fbbe206183bb5410f95ac33bb814f06ab
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-02-16 13:52:20 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 function App() {
-  return <div className="text-3xl font-bold underline primary-100">app</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
